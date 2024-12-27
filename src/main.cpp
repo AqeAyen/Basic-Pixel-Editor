0916b911ee4651ce829b5b60f97e7c55f1658c88
AqeAyen <kervgamingph@gmail.com>
2024-12-27 14:27:42 +0800

    added a basic drawing function
    So I now added a very basic drawing function plus a pixel struct, the
    function works by storing the current position of the mouse in the pixel
    vector and then drawing it now by DrawRectangleRec but via
    DrawRectangle. Also I forgot to create a new branch for feature testing
    so I lm gonna do that later

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,29 @@
 #include "raylib.h"
+#include <stdio.h>
+#include <vector>
+struct Pixel {
+  float x, y;
+  float width, height;
+  Color color;
+};
 
+void UserDraw(float width, float height, Color color,
+              std::vector<Pixel> &pixels) {
+  if (IsMouseButtonDown(MOUSE_LEFT_BUTTON)) {
+    pixels.push_back(
+        {(float)GetMouseX(), (float)GetMouseY(), width, height, color});
+  }
+  for (const auto &pixel : pixels) {
+    DrawRectangle(pixel.x, pixel.y, pixel.width, pixel.height, pixel.color);
+  }
+}
 int main(int argc, char **argv) {
   const int SCREEN_WIDTH = 640;
   const int SCREEN_HEIGHT = 400;
 
   InitWindow(SCREEN_WIDTH, SCREEN_WIDTH, "Pixel");
+
+  std::vector<Pixel> pixels;
 
   SetTargetFPS(60);
 
@@ -14,6 +33,7 @@ int main(int argc, char **argv) {
     BeginDrawing(); // We Draw stuff inside m here
 
     ClearBackground(WHITE);
+    UserDraw(32, 32, BLACK, pixels);
 
     EndDrawing();
   }
